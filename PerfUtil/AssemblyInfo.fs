1ab4adb36c767b2ca9729ceadeab8a753d55c729
Henrik <henrik@haf.se>
2014-04-18 14:10:00 +0200

    separating nunit from assembly

--- PerfUtil/AssemblyInfo.fs
+++ PerfUtil/AssemblyInfo.fs
@@ -1,6 +1,8 @@
 ﻿namespace PerfUtil
 
     open System.Reflection
+    open System.Runtime.CompilerServices
 
     [<assembly:AssemblyVersion("0.1.4.*")>]
+    [<assembly:InternalsVisibleTo("PerfUtil.NUnit")>]
     do()
